4625dd4b03835339f404aed2a03521f5ed607b1a
adriengaillet <32905696+adriengaillet@users.noreply.github.com>
2020-03-03 12:47:33 +0100

    Add files via upload

---
+++ Clima/Clima/API.swift
@@ -0,0 +1,11 @@
+//
+//  API.swift
+//  Clima
+//
+//  Created by Adrien on 02/03/2020.
+//  Copyright © 2020 Adrien GAILLET. All rights reserved.
+//
+
+import Foundation
+
+let APIKey: String = "YOUR_API_KEY"
